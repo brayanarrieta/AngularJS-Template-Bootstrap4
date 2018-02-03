bbf6b2a7d9faa37aa09c1f51ecae8c96edc76796
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2018-02-03 11:20:58 -0600

    Initial Files

---
+++ app/common/constants/constants.js
@@ -0,0 +1,7 @@
+'use strict';
+(function () {
+    angular.module('app')
+        .constant('APP_AUTHOR', 'Brayan Arrieta')
+        .constant('APP_NAME', 'AngularJS-Template-Bootstrap4')
+        .constant('APP_VERSION', '0.0.0');
+})();
---
+++ app/core/layouts/asideController.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Arrieta on 21/11/2017.
+ */
+'use strict';
+angular.module('app').controller('AsideController', function($scope, $location, $interval,$http) {
+});
